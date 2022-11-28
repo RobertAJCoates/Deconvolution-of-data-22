de9f5cd3c390ebab1fdf024662f4d4492556cb08
Robert Coates <59651383+RobertAJCoates@users.noreply.github.com>
2022-11-28 00:01:13 +0000

    .py brute force update

--- pybruteforce/stepfn.py
+++ pybruteforce/stepfn.py
@@ -1,4 +1,5 @@
 import numpy as np, matplotlib.pyplot as plt
+from cmath import exp,pi
 #test fn: step fn; create fn using np arrays
 n=16
 x = np.arange(0,n,1)
@@ -6,11 +7,12 @@ y =np.zeros(n)
 for i in np.arange(3,13,1):
     y[i] = 2
 #calculate convolution of step fn (F is fourier) 
-t_fftfreq = np.fft.fftfreq(n,d = 1/x.size)
+#t[0] = exp(-2*pi*x[1]),....t[n]= exp(-2*pi*x[n]) 
+# print(t)
 x_fft = np.fft.fft(x)
-t_fft = x_fft * t_fftfreq#--->g(t)
+t_fft = x_fft * exp(-2*pi)
 y_fft = np.fft.fft(y)#--->f(x)
-ytfft = y_fft * x_fft #---> F(f*g) = Ff * Fg ---->convolution 
+ytfft = y_fft * t_fft #---> F(f*g) = Ff * Fg ---->convolution 
 Fyt = np.fft.ifftshift(np.round(np.real(ytfft)))#--->ifftshift [inverse fftshift]: shifts zero freq component 2 centre of spectrum
 
 #add guassian noise
