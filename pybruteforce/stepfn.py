bced7d40da844677d6259f68001bf5547603688e
Robert Coates <59651383+RobertAJCoates@users.noreply.github.com>
2022-12-02 14:27:58 +0000

    stepfn.py update

--- pybruteforce/stepfn.py
+++ pybruteforce/stepfn.py
@@ -1,22 +1,21 @@
 import numpy as np, matplotlib.pyplot as plt
-from cmath import exp,pi
 #test fn: step fn; create fn using np arrays
-n=16
+n=100
+freq = n/np.pi
+#def x and f(x)---->call y
 x = np.arange(0,n,1)
 y =np.zeros(n)
-for i in np.arange(3,13,1):
-    y[i] = 2
+for i in np.arange(25,75):
+    y[i] = 1
+#def t and g(t)-->call u
+t = np.arange(0,np.pi,1/(freq))
+u = (np.sin((t)))**2
+print(x.size,t.size)
 #calculate convolution of step fn (F is fourier) 
-#write step fn above y' in terms of t instead of x and verfiy y',t equic to y,x
-freq = x.size/(2*pi)
-print(freq)
-t = np.zeros(0)
-for i in np.arange(0,n,1):    
-    t[i] = (x[i] * 2 * pi)
-    
-t_fft = np.fft.fft(t)
 y_fft = np.fft.fft(y)#--->f(x)
-ytfft = y_fft * t_fft #---> F(f*g) = Ff(x) * Fg(t) ---->convolution 
+u_fft = np.fft.fft(u)#--->g(t)
+ytfft = y_fft * u_fft #---> F(f*g) = Ff(x) * Fg(t) ---->convolution
+print(ytfft)
 Fyt = np.fft.ifftshift(np.round(np.real(ytfft)))#--->ifftshift [inverse fftshift]: shifts zero freq component 2 centre of spectrum
 #add guassian noise
 
@@ -25,6 +24,7 @@ Fyt = np.fft.ifftshift(np.round(np.real(ytfft)))#--->ifftshift [inverse fftshift
 #plot graphs (stepfn, convoluted stepfn, convoluted stepfn with gaussain)
 figs,axs = plt.subplots(2,2)
 axs[0,0].step(x,y)
-axs[0,1].plot(Fyt)
+axs[1,0].plot(t,u)
+axs[0,1].plot(ytfft)
 plt.savefig('stepfntest.png')
 plt.show()
