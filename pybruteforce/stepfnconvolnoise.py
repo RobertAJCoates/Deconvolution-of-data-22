4d761ce6b59e7a3ccda7a0a6053f0502c064d31f
Robert Coates <59651383+RobertAJCoates@users.noreply.github.com>
2022-12-04 23:45:05 +0000

    update brute force branch

--- pybruteforce/stepfnconvolnoise.py
+++ pybruteforce/stepfnconvolnoise.py
@@ -7,12 +7,12 @@ print(xstepfn.size,ystepfn.size,t,u1)
 #convolutestepfn
 convol_stepfn = continuousFourierconvol(ystepfn,u1)#--->contFourier used cos g1(t) is a cont. fn
 #add guass noise
-convol_stepfn_plusnoise = ()
+#convol_stepfn_plusnoise = ()
 #plot graphs
 figs,axs =plt.subplots(2,2)
 axs[0,0].step(xstepfn,ystepfn)
 axs[0,0].plot(t,u1)
 axs[0,1].plot(convol_stepfn)
-axs[1,1].plot(convol_stepfn_plusnoise)
+#axs[1,1].plot(convol_stepfn_plusnoise)
 plt.savefig("stepfnbconvol.png")
 plt.show()
